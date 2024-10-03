4b375dc8a0457dbd69a71d66cc2724a6435255d0
ANKIT143M <shubhamm123645@gmail.com>
2024-10-03 14:30:10 +0530

    add errorCapture in tag, productTag controller

--- controllers/productTag.mjs
+++ controllers/productTag.mjs
@@ -1,22 +1,23 @@
+import { CustomError, errorCapture } from "../error.mjs";
 import { ProductTag } from "../model/productTag.mjs";
 
-const createProductTag = async (req, res)=>{
+const createProductTag = errorCapture(async (req, res, next)=>{
     const pt = new ProductTag({tag_id:req.body.tag_id, product_id:req.body.product_id});
     await pt.save();
     if (pt) {
         res.json({msg:"ProductTag Create Successfully", productTag:pt});
         return
     };
-    res.send("ProductTag not created");
-};
+    throw new CustomError(null, 500, "ProductTag not created");
+});
 
-const getAllProductTag = async (req, res)=>{
+const getAllProductTag = errorCapture(async (req, res, next)=>{
     const productTag = await ProductTag.find();
     if (productTag.length > 0) {
         res.json({msg:"Get all product tag success", productTags:productTag});
         return
     };
-    
-};
+    throw new CustomError(null, 404, "Product Tag not found");
+});
 
 export{createProductTag, getAllProductTag}
